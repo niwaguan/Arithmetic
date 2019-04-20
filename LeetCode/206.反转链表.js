48f61f49e1af5d666f65c7a340bbe80ec77352b7
Gyang <goyaya@yeah.net>
2019-04-20 15:47:35 +0800

    [206] 反转链表 JavaScript

---
+++ LeetCode/206.反转链表.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (60.07%)
+ * Total Accepted:    51K
+ * Total Submissions: 84.6K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+    var pre = null
+    while(head !== null) {
+        const next = head.next
+        head.next = pre
+        pre = head
+        head = next
+    }
+
+    return pre
+};
+
