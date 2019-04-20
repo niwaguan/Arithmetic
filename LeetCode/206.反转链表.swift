59d5a9f753debedc2d17ddefd378c7d680268c17
Gyang <goyaya@yeah.net>
2019-04-20 15:54:35 +0800

    [206] 反转链表 Swift

---
+++ LeetCode/206.反转链表.swift
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=swift
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (60.30%)
+ * Total Accepted:    51K
+ * Total Submissions: 84.6K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+
+// Definition for singly-linked list.
+//public class ListNode {
+//    public var val: Int
+//    public var next: ListNode?
+//    public init(_ val: Int) {
+//        self.val = val
+//        self.next = nil
+//    }
+//}
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        
+        var pre: ListNode?
+        var reversedHead = head
+        while reversedHead != nil {
+            let next = reversedHead?.next
+            
+            reversedHead?.next = pre
+            pre = reversedHead
+            reversedHead = next
+        }
+        
+        return pre
+    }
+}
+
