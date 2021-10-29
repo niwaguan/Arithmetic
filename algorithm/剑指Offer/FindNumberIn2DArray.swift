e7433544cbdd417fb993c862e90a3ec76c843f3f
niwaguan <niwaguan@gmail.com>
2021-10-29 15:05:09 +0800

    feat: 二维数组查找 Plus。右上角开始搜索

--- algorithm/剑指Offer/FindNumberIn2DArray.swift
+++ algorithm/剑指Offer/FindNumberIn2DArray.swift
@@ -18,65 +18,25 @@ import Foundation
  ]
  */
 class FindNumberIn2DArray {
-    struct Index {
-        let line: Int
-        let column: Int
-    }
-    
     func findNumberIn2DArray(_ matrix: [[Int]], _ target: Int) -> Bool {
-        func search(_ array: inout [[Int]], low: Index, up: Index) -> Bool {
-            if low.line <= up.line && low.column <= up.column {
-                let middle = Index(line: (low.line + up.line) / 2, column: (low.column + up.column) / 2)
-                // 交叉搜索
-                for i in low.column...up.column {
-                    let value = array[middle.line][i]
-                    if value == target {
-                        return true
-                    }
-                }
-                
-                for i in low.line...up.line {
-                    let value = array[i][middle.column]
-                    if value == target {
-                        return true
-                    }
-                }
-                
-                var ret = false
-                let value = matrix[middle.line][middle.column]
-                let topLeft = (low: low, up: Index(line: middle.line - 1, column: middle.column - 1))
-                let topRight = (low: Index(line: low.line, column: middle.column + 1), up: Index(line: middle.line - 1, column: up.column))
-                let bottomLeft = (low: Index(line: middle.line + 1, column: low.column), up: Index(line: up.line, column: middle.column - 1))
-                let bottomRight = (low: Index(line: middle.line + 1, column: middle.column + 1), up: up)
-                
-                // 被middle分隔的四个角落
-                ret = search(&array, low: topRight.low, up: topRight.up)
-                if ret {
-                    return true
-                }
-                
-                ret = search(&array, low: bottomLeft.low, up: bottomLeft.up)
-                if ret {
-                    return true
-                }
-                
-                if value < target {
-                    ret = search(&array, low: bottomRight.low, up: bottomRight.up)
-                } else {
-                    ret = search(&array, low: topLeft.low, up: topLeft.up)
-                }
-                return ret
+        let rows = matrix.count
+        guard let row0 = matrix.first else {
+            return false
+        }
+        let columns = row0.count
+        
+        var row = 0, colomn = columns - 1
+        while row >= 0 && row < rows && colomn >= 0 && colomn < columns {
+            let value = matrix[row][colomn]
+            if value == target {
+                return true
+            } else if value < target {
+                row += 1
+            } else {
+                colomn -= 1
             }
-            return false
         }
         
-        let lines = matrix.count
-        guard let line = matrix.first else {
-            return false
-        }
-        let columns = line.count
-        var values = matrix
-        
-        return search(&values, low: Index(line: 0, column: 0), up: Index(line: lines - 1, column: columns - 1))
+        return false
     }
 }
